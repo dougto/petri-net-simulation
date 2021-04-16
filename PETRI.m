f3a424b877b60dd4f7a3fbe6c61480568f6752d9
Douglas Toledo <douglas22toledo@gmail.com>
2021-04-15 21:19:26 -0300

    check if node exists

--- PETRI.m
+++ PETRI.m
@@ -9,12 +9,12 @@ function PETRI()
 
   root_node = node(marking);
 
-  calculateMarkings(transitions_input, transitions_output, marking, initial_height, max_height, root_node)
+  calculateMarkings(transitions_input, transitions_output, marking, initial_height, max_height, root_node, root_node)
 
   tree.plot(root_node);
-endfunction
+end
 
-function calculateMarkings(transitions_input, transitions_output, marking, height, max_height, current_node)
+function calculateMarkings(transitions_input, transitions_output, marking, height, max_height, current_node, petri_tree)
   transitions_number = size(transitions_input)(1);
   places_number = size(transitions_input)(2);
 
@@ -26,24 +26,26 @@ function calculateMarkings(transitions_input, transitions_output, marking, heigh
     for k = 1:places_number
       if marking(k) < transitions_input(j,k)
         transition_is_enabled = 0;
-      endif
-    endfor
+      end
+    end
 
     if transition_is_enabled == 1
       new_marking = zeros(1, places_number);
 
       new_marking = marking - transitions_input(j,:) + transitions_output(j,:);
 
-      new_node = node(new_marking);
+      if tree.nodeExists(petri_tree, new_marking) == 0
+        new_node = node(new_marking);
 
-      current_node.Children(j) = new_node;
+        current_node.Children(j) = new_node;
 
-      if height == max_height
-        disp("reached maximum search height")
-        return
-      endif
+        if height == max_height
+          disp("reached maximum search height")
+          return
+        end
 
-      calculateMarkings(transitions_input, transitions_output, new_marking, height, max_height, new_node);
-    endif
-  endfor
-endfunction
+        calculateMarkings(transitions_input, transitions_output, new_marking, height, max_height, new_node, petri_tree);
+      end
+    end
+  end
+end
--- tree.m
+++ tree.m
@@ -15,8 +15,32 @@ classdef tree
         for i = 1:numberOfchildren
           if !isempty(node.Children{1,i})
             printMarking(node.Children{1,i});
-          endif
+          end
         end
+      end
+    end
+
+    function r = nodeExists(parent_node, marking)
+      r = internalNodeExists(parent_node, marking);
+
+      function r = internalNodeExists(parent_node, intMarking)
+        if parent_node.Marking == intMarking
+          r = 1;
+          return
+        end
+
+        numberOfchildren = size(parent_node.Children)(2);
+
+        for i = 1:numberOfchildren
+          if !isempty(parent_node.Children{1,i})
+            r = internalNodeExists(parent_node.Children{1,i}, intMarking);
+            if r == 1
+              return
+            end
+          end
+        end
+
+        r = 0;
       end
     end
   end
